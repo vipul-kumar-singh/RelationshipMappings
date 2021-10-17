5bfd31ff646d2b521683d57c3abd694e62869a7d
Vipul Kumar Singh <vipul24.9669@gmail.com>
2021-10-17 16:17:58 +0530

    Initial Commit

---
+++ src/main/java/com/vkstech/relationships/EntityRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.vkstech.relationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityRelationshipsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityRelationshipsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vkstech/relationships/EntityRelationshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkstech.relationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityRelationshipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
